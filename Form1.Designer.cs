a192badccb9e4b92c6a0cfa75d7f55a7241e12ff
esm720 <esm720@gmail.com>
2022-08-13 17:53:30 -0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Activity_13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TLLabel = new System.Windows.Forms.Label();
+            this.TMLabel = new System.Windows.Forms.Label();
+            this.TRLabel = new System.Windows.Forms.Label();
+            this.MLLabel = new System.Windows.Forms.Label();
+            this.MMLabel = new System.Windows.Forms.Label();
+            this.MRLabel = new System.Windows.Forms.Label();
+            this.BLLabel = new System.Windows.Forms.Label();
+            this.BMLabel = new System.Windows.Forms.Label();
+            this.BRLabel = new System.Windows.Forms.Label();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.NewGameButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TLLabel
+            // 
+            this.TLLabel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.TLLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TLLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TLLabel.Location = new System.Drawing.Point(42, 75);
+            this.TLLabel.Name = "TLLabel";
+            this.TLLabel.Size = new System.Drawing.Size(70, 67);
+            this.TLLabel.TabIndex = 0;
+            this.TLLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TMLabel
+            // 
+            this.TMLabel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.TMLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TMLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TMLabel.Location = new System.Drawing.Point(150, 75);
+            this.TMLabel.Name = "TMLabel";
+            this.TMLabel.Size = new System.Drawing.Size(70, 67);
+            this.TMLabel.TabIndex = 1;
+            this.TMLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TRLabel
+            // 
+            this.TRLabel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.TRLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TRLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TRLabel.Location = new System.Drawing.Point(271, 75);
+            this.TRLabel.Name = "TRLabel";
+            this.TRLabel.Size = new System.Drawing.Size(70, 67);
+            this.TRLabel.TabIndex = 2;
+            this.TRLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MLLabel
+            // 
+            this.MLLabel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.MLLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.MLLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MLLabel.Location = new System.Drawing.Point(42, 184);
+            this.MLLabel.Name = "MLLabel";
+            this.MLLabel.Size = new System.Drawing.Size(70, 67);
+            this.MLLabel.TabIndex = 3;
+            this.MLLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MMLabel
+            // 
+            this.MMLabel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.MMLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.MMLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MMLabel.Location = new System.Drawing.Point(150, 184);
+            this.MMLabel.Name = "MMLabel";
+            this.MMLabel.Size = new System.Drawing.Size(70, 67);
+            this.MMLabel.TabIndex = 4;
+            this.MMLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MRLabel
+            // 
+            this.MRLabel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.MRLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.MRLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MRLabel.Location = new System.Drawing.Point(271, 184);
+            this.MRLabel.Name = "MRLabel";
+            this.MRLabel.Size = new System.Drawing.Size(70, 67);
+            this.MRLabel.TabIndex = 5;
+            this.MRLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BLLabel
+            // 
+            this.BLLabel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BLLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.BLLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BLLabel.Location = new System.Drawing.Point(42, 299);
+            this.BLLabel.Name = "BLLabel";
+            this.BLLabel.Size = new System.Drawing.Size(70, 67);
+            this.BLLabel.TabIndex = 6;
+            this.BLLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BMLabel
+            // 
+            this.BMLabel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BMLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.BMLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BMLabel.Location = new System.Drawing.Point(150, 299);
+            this.BMLabel.Name = "BMLabel";
+            this.BMLabel.Size = new System.Drawing.Size(70, 67);
+            this.BMLabel.TabIndex = 7;
+            this.BMLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BRLabel
+            // 
+            this.BRLabel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BRLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.BRLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BRLabel.Location = new System.Drawing.Point(271, 299);
+            this.BRLabel.Name = "BRLabel";
+            this.BRLabel.Size = new System.Drawing.Size(70, 67);
+            this.BRLabel.TabIndex = 8;
+            this.BRLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.ResultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResultLabel.Location = new System.Drawing.Point(42, 396);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(299, 39);
+            this.ResultLabel.TabIndex = 9;
+            this.ResultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // NewGameButton
+            // 
+            this.NewGameButton.Location = new System.Drawing.Point(42, 451);
+            this.NewGameButton.Name = "NewGameButton";
+            this.NewGameButton.Size = new System.Drawing.Size(130, 69);
+            this.NewGameButton.TabIndex = 10;
+            this.NewGameButton.Text = "New Game";
+            this.NewGameButton.UseVisualStyleBackColor = true;
+            this.NewGameButton.Click += new System.EventHandler(this.NewGameButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(212, 451);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(129, 69);
+            this.ExitButton.TabIndex = 11;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 532);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.NewGameButton);
+            this.Controls.Add(this.ResultLabel);
+            this.Controls.Add(this.BRLabel);
+            this.Controls.Add(this.BMLabel);
+            this.Controls.Add(this.BLLabel);
+            this.Controls.Add(this.MRLabel);
+            this.Controls.Add(this.MMLabel);
+            this.Controls.Add(this.MLLabel);
+            this.Controls.Add(this.TRLabel);
+            this.Controls.Add(this.TMLabel);
+            this.Controls.Add(this.TLLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TLLabel;
+        private System.Windows.Forms.Label TMLabel;
+        private System.Windows.Forms.Label TRLabel;
+        private System.Windows.Forms.Label MLLabel;
+        private System.Windows.Forms.Label MMLabel;
+        private System.Windows.Forms.Label MRLabel;
+        private System.Windows.Forms.Label BLLabel;
+        private System.Windows.Forms.Label BMLabel;
+        private System.Windows.Forms.Label BRLabel;
+        private System.Windows.Forms.Label ResultLabel;
+        private System.Windows.Forms.Button NewGameButton;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_13
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static Random random = new Random();
+
+        private int randGen()
+        {
+            return random.Next(0,2);
+        }
+
+        private void NewGameButton_Click(object sender, EventArgs e)
+        {
+          ResultLabel.Text = " ";
+
+          int ROW = 3;
+          int COL = 3;
+          int[,] ticTacArray = new int[ROW, COL];
+          string[,] ticTacDisplayArray = new string[ROW, COL]; 
+         
+
+            for(int row = 0; row < ROW; row++)
+            {
+                for(int col = 0; col < COL; col++)
+                {
+                    ticTacArray[row, col] = randGen();
+                    if (ticTacArray[row,col] == 0)
+                    {
+                        ticTacDisplayArray[row, col] = "X";
+                    }
+                    else
+                    {
+                        ticTacDisplayArray[row, col] = "O";
+                    }
+                }
+            }
+
+            TLLabel.Text = ticTacDisplayArray[0, 0];
+            TMLabel.Text = ticTacDisplayArray[0, 1];
+            TRLabel.Text = ticTacDisplayArray[0, 2];
+            MLLabel.Text = ticTacDisplayArray[1, 0];
+            MMLabel.Text = ticTacDisplayArray[1, 1];
+            MRLabel.Text = ticTacDisplayArray[1, 2];
+            BLLabel.Text = ticTacDisplayArray[2, 0];
+            BMLabel.Text = ticTacDisplayArray[2, 1];
+            BRLabel.Text = ticTacDisplayArray[2, 2];
+
+            checkWin();
+        }
+
+        private void checkWin()
+        {
+            int xPlayerWins = 0;
+            int yPlayerWins = 0;
+            //HORIZONTAL WINS
+            if (TLLabel.Text.Equals("X") && TMLabel.Text.Equals("X") && TRLabel.Text.Equals("X"))//
+            {
+                xPlayerWins++;
+            }
+            else if (TLLabel.Text.Equals("O") && TMLabel.Text.Equals("O") && TRLabel.Text.Equals("O"))
+            {
+                yPlayerWins++;
+            }
+            if (MLLabel.Text.Equals("X") && MMLabel.Text.Equals("X") && MRLabel.Text.Equals("X"))//
+            {
+                xPlayerWins++;
+            }
+            else if (MLLabel.Text.Equals("O") && MMLabel.Text.Equals("O") && MRLabel.Text.Equals("O"))
+            {
+                yPlayerWins++;
+            }
+            if (BLLabel.Text.Equals("X") && BMLabel.Text.Equals("X") && BRLabel.Text.Equals("X"))
+            {
+                xPlayerWins++;
+            }
+            else if (BLLabel.Text.Equals("O") && BMLabel.Text.Equals("O") && BRLabel.Text.Equals("O"))
+            {
+                yPlayerWins++;
+            }
+            //VERTICAL WINS
+            if (TLLabel.Text.Equals("X") && MLLabel.Text.Equals("X") && BLLabel.Text.Equals("X"))
+            {
+                xPlayerWins++;
+            }
+            else if (TLLabel.Text.Equals("O") && MLLabel.Text.Equals("O") && BLLabel.Text.Equals("O"))
+            {
+                yPlayerWins++;
+            }
+            if (TMLabel.Text.Equals("X") && MMLabel.Text.Equals("X") && BMLabel.Text.Equals("X"))
+            {
+                xPlayerWins++;
+            }
+            else if (TMLabel.Text.Equals("O") && MMLabel.Text.Equals("O") && BMLabel.Text.Equals("O"))
+            {
+                yPlayerWins++;
+            }
+            if (TRLabel.Text.Equals("X") && MRLabel.Text.Equals("X") && BRLabel.Text.Equals("X"))
+            {
+                xPlayerWins++;
+            }
+            else if (TRLabel.Text.Equals("O") && MRLabel.Text.Equals("O") && BRLabel.Text.Equals("O"))
+            {
+                yPlayerWins++;
+            }
+            //DIAGNAL WINS
+
+            if (TLLabel.Text.Equals("X") && MMLabel.Text.Equals("X") && BRLabel.Text.Equals("X"))
+            {
+                xPlayerWins++;
+            }
+            else if (TLLabel.Text.Equals("O") && MMLabel.Text.Equals("O") && BRLabel.Text.Equals("O"))
+            {
+                yPlayerWins++;
+            }
+            if (TRLabel.Text.Equals("X") && MMLabel.Text.Equals("X") && BLLabel.Text.Equals("X"))
+            {
+                xPlayerWins++;
+            }
+            else if (TRLabel.Text.Equals("O") && MMLabel.Text.Equals("O") && BLLabel.Text.Equals("O"))
+            {
+                yPlayerWins++;
+            }
+            //Compare Wins
+            if(xPlayerWins > yPlayerWins)
+            {
+                ResultLabel.Text = "X Player Wins";
+            }
+            else if (xPlayerWins < yPlayerWins)
+            {
+                ResultLabel.Text = "O Player Wins";
+            }
+            else
+            {
+                ResultLabel.Text = "It's a Tie";
+            }
+           // MessageBox.Show("X: " + xPlayerWins + "\n Y: " + yPlayerWins); USE FOR DEBUGGING WIN COUNT
+
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
